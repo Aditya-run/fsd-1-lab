00772aa700c75e106c31380539fc339095b2760d
Aditya-run <adityast473@gmail.com>
2026-02-02 13:56:36 +0530

    Experiment - 2 is added

---
+++ DOM/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+let heading = document.querySelector("#heading")
+console.log(heading)
+
+let firstElement = document.querySelector(".paragraph")
+console.log(firstElement)
+let para = document.querySelectorAll(".paragraph")
+console.log(para)
+let tag = document.querySelectorAll("p")
+
+console.log(tag)
