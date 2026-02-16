50556e3345efe80cce6d7ec03962a0e1ade18b46
Aditya-run <adityast473@gmail.com>
2026-02-16 14:10:10 +0530

    added Experiment 3.1

---
+++ learningComponents/src/App.jsx
@@ -0,0 +1,14 @@
+import About from './Components/About';
+import Home from './Components/Home';
+import Contact from './Components/Contact'
+function App(){
+  return (
+    <div>
+      <Home/>
+      
+      <About/>
+      <Contact/>
+    </div>
+  )
+};
+export default App;
